d52c037b793ee4d73f4cff4a39043f622a0c3579
Melvin Omega <melvinomega151@gmail.com>
2021-09-18 16:59:39 +0300

    modify config file

--- config.py
+++ config.py
@@ -1,5 +1,28 @@
 import os
 
+from flask_migrate import Config
+
 class config:
 
     MAIL_SERVER = 'smtp.googlemail.com'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}    
