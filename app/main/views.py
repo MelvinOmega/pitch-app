c60140c54cceb005bb373f2f9db5111061132af6
Melvin Omega <melvinomega151@gmail.com>
2021-09-18 17:04:47 +0300

    add upvote and downvote sections

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main
-from ..models import User, Pitch, Comment
+from ..models import User, Pitch, Comment, Upvote,Downvote
 from .forms import PitchForm, UpdateProfile, CommentsForm
 from flask_login import login_required, current_user
 from .. import db, photos
@@ -16,10 +16,10 @@ def index():
     pickup_lines = Pitch.query.filter_by(category = 'Pickup Lines').all()
     interview_pitch = Pitch.query.filter_by(category = 'Interview Pitch').all()
     promotion_pitch = Pitch.query.filter_by(category = 'Promotion Pitch').all()
-    love_pitch = Pitch.query.filter_by(category = 'Love Pitch').all()
+    advertisement = Pitch.query.filter_by(category = 'advertisement').all()
 
 
-    return render_template('index.html', title = title,product_pitch = product_pitch, pickup_lines = pickup_lines, interview_pitch = interview_pitch, promotion_pitch = promotion_pitch, love_pitch = love_pitch)
+    return render_template('index.html', title = title,product_pitch = product_pitch, pickup_lines = pickup_lines, interview_pitch = interview_pitch, promotion_pitch = promotion_pitch, advertisement = advertisement)
 
 @main.route('/user/<uname>')
 @login_required
@@ -94,3 +94,35 @@ def comments():
         return redirect(url_for('main.comments'))
     
     return render_template('comments.html', comments_form = comments_form, comments = comments)
+
+@main.route('/like/<int:id>',methods = ['POST','GET'])
+@login_required
+def like(id):
+    get_pitches = Upvote.get_upvotes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for pitch in get_pitches:
+        to_str = f'{pitch}'
+        print(valid_string+" "+to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.index',id=id))
+        else:
+            continue
+    new_vote = Upvote(user = current_user, pitch_id=id)
+    new_vote.save()
+    return redirect(url_for('main.index',id=id))
+
+@main.route('/dislike/<int:id>',methods = ['POST','GET'])
+@login_required
+def dislike(id):
+    pitch = Downvote.get_downvotes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for p in pitch:
+        to_str = f'{p}'
+        print(valid_string+" "+to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.index',id=id))
+        else:
+            continue
+    new_downvote = Downvote(user = current_user, pitch_id=id)
+    new_downvote.save()
+    return redirect(url_for('main.index',id = id))    
